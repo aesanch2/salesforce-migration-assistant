c27ce29d71a993efce1ae0a6256331f8dfbf33f2
Anthony Sanchez <aesanch2@asu.edu>
2015-02-26 13:09:49 -0700

    Refine SMAPackage and create tests for it

--- src/main/java/org/asu/sma/SMAPackage.java
+++ src/main/java/org/asu/sma/SMAPackage.java
@@ -8,54 +8,83 @@ import java.util.ArrayList;
  */
 public class SMAPackage {
     private String destination;
-    private ArrayList<String> memberList;
+    private String workspace;
+    private ArrayList<String> contents;
     private boolean destructiveChange;
+    //Project configuration variables
     private boolean validateOnly;
     private boolean generateUnitTests;
+    private String username;
+    private String password;
+    private String server;
     //Global configuration variables
     private String pluginHome;
     private String runTestRegex;
     private String pollWait;
+    private String maxPoll;
 
     /**
      * Constructor for manifest SMAPackage
      * @param destination
-     * @param memberList
+     * @param contents
      * @param destructiveChange
-     * @param validateOnly
      */
-    public SMAPackage(String destination, ArrayList<String> memberList,
-                      boolean destructiveChange, boolean validateOnly){
+    public SMAPackage(String destination, ArrayList<String> contents,
+                      boolean destructiveChange){
         this.destination = destination;
-        this.memberList = memberList;
+        this.contents = contents;
         this.destructiveChange = destructiveChange;
-        this.validateOnly = validateOnly;
+
+        //Modify the location of the SMAPackage based on what xml file we're representing.
+        if(destructiveChange){
+            this.destination = destination + "/src/destructiveChanges.xml";
+        } else {
+            this.destination = destination + "/src/package.xml";
+        }
     }
 
     /**
      * Constructor for build SMAPackage
      * @param destination
-     * @param memberList
+     * @param contents
      * @param jenkinsHome
      * @param runTestRegex
      * @param pollWait
      * @param generateUnitTests
+     * @param validateOnly
      */
-    public SMAPackage(String destination, ArrayList<String> memberList, String jenkinsHome,
-                      String runTestRegex, String pollWait, boolean generateUnitTests){
-        this.destination = destination;
-        this.memberList = memberList;
+    public SMAPackage(String destination, ArrayList<String> contents, String jenkinsHome,
+                      String runTestRegex, String pollWait, String maxPoll,
+                      boolean generateUnitTests, boolean validateOnly,
+                      String username, String password, String server){
+        this.contents = contents;
         this.runTestRegex = runTestRegex;
         this.pollWait = pollWait;
+        this.maxPoll = maxPoll;
         this.generateUnitTests = generateUnitTests;
+        this.validateOnly = validateOnly;
+        this.username = username;
+        this.server = server;
+
+        if (password.isEmpty()){
+            this.password = "{$sf.password}";
+        }else{
+            this.password = password;
+        }
 
         //SMA's plugin directory for locating the ant-salesforce.jar
         pluginHome = jenkinsHome + "/plugins/sma";
+
+        //Modify the location of the SMAPackage for the build file
+        this.destination = destination + "/build/build.xml";
+        this.workspace = destination + "/src";
     }
 
-    public ArrayList<String> getMemberList() { return memberList; }
-
     public String getDestination() { return destination; }
+
+    public String getWorkspace() { return workspace; }
+
+    public ArrayList<String> getContents() { return contents; }
 
     public boolean isDestructiveChange() { return destructiveChange; }
 
@@ -68,4 +97,13 @@ public class SMAPackage {
     public String getRunTestRegex() { return runTestRegex; }
 
     public String getPollWait() { return pollWait; }
+
+    public String getMaxPoll() { return maxPoll; }
+
+    public String getUsername() { return username; }
+
+    public String getServer() { return server; }
+
+    public String getPassword() { return password; }
+
 }
